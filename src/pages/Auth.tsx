cc0e814e9313decc2850ef18ef35a9791848e10e
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-04-19 10:19:53 +0530

    restructure

--- src/pages/Auth.tsx
+++
@@ -1,7 +0,0 @@
-const Auth = () => {
-  return (
-    <div className="min-h-dvh flex items-center justify-center">Auth</div>
-  )
-}
-
-export default Auth
--- src/pages/NotFound.tsx
+++
@@ -1,7 +0,0 @@
-const NotFound = () => {
-  return (
-    <div className="min-h-dvh flex items-center justify-center">NotFound</div>
-  )
-}
-
-export default NotFound
